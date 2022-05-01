9507abd01e37a9c7d86c46be8248a6203ede45f9
JohnNadja <cv3802@innovaccion.mx>
2022-05-01 09:55:33 -0500

    Realizando cambios a FizzBuzzService.js para 'applyValidationInExplorer'

--- fizzbuz/lib/services/FizzBuzzService.js
+++ fizzbuz/lib/services/FizzBuzzService.js
@@ -1,21 +1,40 @@
 class FizzBuzzService{
     static applyValidationInExplorer(explorer){
         if (explorer.score % 3 === 0){
-            const explorer3 = {name: "Explorer3", score: 3, trick: "FIZZ"}
-            FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+            explorer.trick = "FIZZ"
+
         }
         else if(explorer.score % 5 === 0){
-            const explorer5 = {name: "Explorer5", score: 5, trick: "BUZZ"}
-            FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+            explorer.trick = "BUZZ"
+    
         }
         else if (explorer.score % 3 === 0 && explorer.score % 5 === 0){
-        const explorer15 = {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
-        FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+            explorer.trick = "FIZZBUZZ"
+
         }
         else{
-        const explorer1 = {name: "Explorer1", score: 1, trick: 1}
-        FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+            explorer.trick = explorer.score
         }
+        return explorer
+    }
+    
+    static applyValidationInExplorer(explorer){
+        if (explorer.score % 3 === 0){
+            explorer.trick = "FIZZ"
+
+        }
+        else if(explorer.score % 5 === 0){
+            explorer.trick = "BUZZ"
+    
+        }
+        else if (explorer.score % 3 === 0 && explorer.score % 5 === 0){
+            explorer.trick = "FIZZBUZZ"
+
+        }
+        else{
+            explorer.trick = explorer.score
+        }
+        return explorer
     }
 }
 
